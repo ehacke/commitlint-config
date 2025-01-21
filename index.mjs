15632f863c39db886a0dee0bff7700dcd9900d43
Eric Hacke <eric.hacke@instacart.com>
2025-01-21 09:31:45 -0500

    fix: add files

---
+++ index.mjs
@@ -0,0 +1,6 @@
+export default {
+	rules: {
+		'type-enum': [2, 'always', ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'merge', 'perf', 'refactor', 'revert', 'style', 'test', 'wip']],
+	},
+	extends: ['@commitlint/config-conventional'],
+};
