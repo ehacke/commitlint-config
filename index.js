d54c7d50ce11015efa3d84e4d01ada016b567a4c
Eric Hacke <eric.hacke@instacart.com>
2025-01-21 09:28:33 -0500

    chore: update dpes

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-	rules: {
-		'type-enum': [2, 'always', ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'merge', 'perf', 'refactor', 'revert', 'style', 'test', 'wip']],
-	},
-	extends: ['@commitlint/config-conventional'],
-};
